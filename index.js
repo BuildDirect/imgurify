f6e6b176d40980ef76def30ab2e9467011bb5688
Erik Oros <erikoros@builddirect.com>
2016-03-18 14:56:07 -0400

    Encoding fixes.

--- index.js
+++ index.js
@@ -1,57 +1,92 @@
-var through    = require('through2')
-var rasterHead = function(i,type) { return i == 0 ? "module.exports = 'data:image/"+getMimeType(type)+";base64," : '' }
-var svgHead    = function(i,type) { return i == 0 ? "module.exports = 'data:image/svg+xml;charset=utf8," : '' }
-var rasterTail = function()       { return "'" }
-var svgTail    = function()       { return "'" }
-var isImg      = function(file)   {
+var fs = require('fs');
+var through = require('through2');
+
+var rasterHead = function(i, type) {
+    return (i == 0) ? "module.exports = 'data:image/" + getMimeType(type) + ";base64," : '';
+};
+
+var svgHead = function(i, type) {
+    return (i == 0) ? "module.exports = 'data:image/svg+xml," : '';
+};
+
+var rasterTail = function() {
+    return "'";
+};
+
+var svgTail = function() {
+    return "'";
+};
+
+var isImg = function(file) {
     return (/\.((lit)?gif|png|jpg|jpeg|svg)$/).exec(file);
-}
-var getMimeType   = function(type) {
-    if (type == 'jpg') return 'jpeg'
-    return type
-}
+};
+
+var getMimeType = function(type) {
+    if (type == 'jpg') {
+        return 'jpeg';
+    }
+
+    return type;
+};
 
 function rasterStream(file, type) {
-    var i = -1
-    var buffers = [] 
+    var bitmap = fs.readFileSync(file);
+    var base64 = new Buffer(bitmap).toString('base64');
+
+    var i = -1;
+    var buffers = [];
+
     return through(
-        function (buf, enc, next) { 
-          i++; 
-          if (i == 0) { this.push(rasterHead(i,type)) } 
-          buffers.push(buf)
-          next() 
+        function(buf, enc, next) {
+            i++;
+
+            if (i == 0) {
+                this.push(rasterHead(i, type));
+            }
+
+            buffers.push(buf);
+
+            next();
         },
-        function (end) { 
-          var totalLength = buffers.reduce(function(length, buf) {
-            return length + buf.length
-          },0)
-          var finalBuffer = Buffer.concat(buffers, totalLength)
-          this.push(finalBuffer.toString('base64'))
-          this.push(rasterTail())
-          end() 
+        function(end) {
+            this.push(base64);
+            this.push(rasterTail());
+
+            end();
         }
-    )
+    );
 }
 
 function svgStream(file, type) {
-    var i = -1
+    var i = -1;
+
     return through(
-        function (buf, enc, next) { 
-          i++; 
-          this.push(svgHead(i,type))
-          this.push(buf.toString('utf-8'))
-          next() 
+        function(buf, enc, next) {
+            i++;
+
+            this.push(svgHead(i, type));
+            this.push(encodeURIComponent(buf.toString('utf-8')));
+
+            next();
         },
-        function (end) { 
-          this.push(svgTail())
-          end() 
+        function(end) {
+            this.push(svgTail());
+
+            end();
         }
-    )
+    );
 }
 
-module.exports = function (file) {
-    if (!isImg(file)) return through()
-    var type = isImg(file)[1]
-    if (type == 'svg') return svgStream(file, type)
-    else return rasterStream(file, type)
+module.exports = function(file) {
+    if (!isImg(file)) {
+        return through();
+    }
+
+    var type = isImg(file)[1];
+
+    if (type == 'svg') {
+        return svgStream(file, type)
+    }
+
+    return rasterStream(file, type)
 }
